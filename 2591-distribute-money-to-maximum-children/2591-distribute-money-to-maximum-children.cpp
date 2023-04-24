9c3f436917f78f68e8dd531cffb9ab889dba6e54
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-04-24 22:48:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (61.46%) - LeetHub

---
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if(money < children) return -1;
+        money -= children;
+
+        int ans = 0;
+        while(money >= 7 && children > 0) {
+            money -= 7;
+            ans++;
+            children--;
+        }
+        if(ans) {
+            if(children == 0 && money > 0) ans -= 1;
+            if(children == 1 && money == 3) ans -= 1;
+        }
+         
+        return ans;
+    }
+};
