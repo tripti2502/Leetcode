9d503e6c11864a73d78f789d3b53d35b917e2a76
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-04-26 20:40:38 +0530

    Time: 7 ms (16.24%), Space: 5.9 MB (92.24%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10) return num;
+        int ans=0;
+        while(num){
+            ans+=num%10;
+            num/=10;
+        }
+        return addDigits(ans);
+    }
+};
