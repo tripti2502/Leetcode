c233aedc76523dd9ea2b1e54fb2d8a84be0a6c9a
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-05-13 13:14:53 +0530

    Time: 23 ms (27.22%), Space: 20.3 MB (14.04%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,41 +1,18 @@
 class Solution {
+    int mod=1e9+7;
+    long long func(int l,int h,int z,int o,long long count,vector<int> &dp ){
+      if(count>=h) return 0;
+      if(dp[count]!=-1) return dp[count];
+      long long ze=func(l,h,z,o,count+z,dp)%mod;
+      if(count+z>=l && count+z<=h) ze+=1;
+      long long one=func(l,h,z,o,count+o,dp)%mod;
+if(count+o >=l && count+o<=h) one+=1;
+      return dp[count]=(ze+one)%mod;
+    }
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high + 1);
-        dp[0] = 1;
-        int res = 0, mod = 1e9 + 7;
-        for (int i = 1; i <= high; ++i) {
-            // if we can add 0 string then add 
-            if (i >= zero) dp[i] = (dp[i] + dp[i - zero]) % mod;
-            // if we can add 1's string i.e i >= len of 1's string
-            if (i >= one) dp[i] = (dp[i] + dp[i - one]) % mod;
-            // if i is in between low and high add 
-            if (i >= low) res = (res + dp[i]) % mod;
-        }
-        return res;
+       long long count=0;
+       vector<int> dp(high+1,-1);
+        return func(low,high,zero,one,count,dp);
     }
 };
-
-
-//wrong ans by chutiya me
-
-// class Solution {
-//     int f(int n, int zero, int one){
-//         if(n==0) return one;
-//         if(n<0) return one;
-//         return f(n-zero, zero, one)+f(n-one, zero, one);
-//     }
-// public:
-//     int countGoodStrings(int low, int high, int zero, int one) {
-//         int ans=0;
-//         int mod=1e9+7;
-//         // string str1 ="", str2="";
-//         for(int i=low; i<=high; i++){
-//             // str1+=zero['0'];
-//             // str2+=one['1'];
-//             ans+=(ans%mod+f(i, zero, one)%mod);
-            
-//         }
-//         return ans;
-//     }
-// };
