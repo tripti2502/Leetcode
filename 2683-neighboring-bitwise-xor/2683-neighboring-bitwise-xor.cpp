d07dd87201b003395fdfacd58bc190a47000073b
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-05-14 09:55:52 +0530

    Time: 335 ms (31.25%), Space: 259.7 MB (25.00%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -1,20 +1,28 @@
+// class Solution {
+// public:
+//     bool doesValidArrayExist(vector<int>& d) {
+//         int n=d.size();
+//         vector<int> v(n);
+//         for(int i=1;i<d.size();i++){
+//             if(d[i-1]==0) v[i]=v[i-1];
+//             else v[i]=!v[i-1];
+//         }
+//        for(int i=0;i<n;i++){
+//            if(i==n-1) {
+//                if((v[i]^v[0])!= d[i]) return false;
+//            }
+//            else {
+//                if((v[i]^v[i+1])!=d[i]) return false;
+//            }
+//        }
+//         return true;
+//     }
+// };
 class Solution {
 public:
-    bool doesValidArrayExist(vector<int>& d) {
-        int n=d.size();
-        vector<int> v(n);
-        for(int i=1;i<d.size();i++){
-            if(d[i-1]==0) v[i]=v[i-1];
-            else v[i]=!v[i-1];
-        }
-       for(int i=0;i<n;i++){
-           if(i==n-1) {
-               if((v[i]^v[0])!= d[i]) return false;
-           }
-           else {
-               if((v[i]^v[i+1])!=d[i]) return false;
-           }
-       }
-        return true;
+    bool doesValidArrayExist(vector<int>& derived) {
+        int ans = 0;
+        for(auto it:derived) ans+=it;
+        return ans%2==0;
     }
 };
