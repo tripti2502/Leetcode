4427127feeb209aacdb9274bb41012600c32e602
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-05-14 09:35:41 +0530

    Time: 403 ms (6.25%), Space: 270.5 MB (25.00%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& d) {
+        int n=d.size();
+        vector<int> v(n);
+        for(int i=1;i<d.size();i++){
+            if(d[i-1]==0) v[i]=v[i-1];
+            else v[i]=!v[i-1];
+        }
+       for(int i=0;i<n;i++){
+           if(i==n-1) {
+               if((v[i]^v[0])!= d[i]) return false;
+           }
+           else {
+               if((v[i]^v[i+1])!=d[i]) return false;
+           }
+       }
+        return true;
+    }
+};
