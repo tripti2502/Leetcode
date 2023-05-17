5aa692d5a1b07e15eaa372cbdd1572e096d7bef6
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-05-17 18:00:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (85.86%) - LeetHub

---
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   int captureForts(vector<int>& forts) {
+    int ans = 0;
+    for (int i = 0, j = 0; i < forts.size(); ++i)
+        if (forts[i] != 0) {
+            if (forts[j] == -forts[i])
+                ans = max(ans, i - j - 1);
+            j = i;
+        }
+    return ans;
+}
+};
