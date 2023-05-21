25eb6b46c1efc0fd038690cdd46d6f0057031773
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-05-21 12:09:07 +0530

    Time: 50 ms (25.00%), Space: 19.9 MB (25.00%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -5,7 +5,7 @@ public:
             reverse(temp.begin(),temp.end());
         string ans="";
         for(int i=0;i<temp.size();i++){
-            if(temp[i]<=s[i]) ans+=temp[i];
+            if(temp[i]<=s[i]) ans.push_back(temp[i]);//ans+=temp[i];
             else ans+=s[i];
         }
         return ans;
