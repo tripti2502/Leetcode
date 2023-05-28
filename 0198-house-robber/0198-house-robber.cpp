16d06d1684086cf34d247585c14d0516aaf464c2
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-05-28 11:20:34 +0530

    Time: 3 ms (36.23%), Space: 7.9 MB (26.48%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int f( int index, vector<int> &nums, vector<int> &dp) {
+        if(index==0)
+            return nums[index];
+        if(index<0)
+            return 0;
+        if(dp[index]!=-1) return dp[index];
+        int pick=nums[index]+f(index-2, nums, dp);
+        int not_pick=0+ f(index-1, nums, dp);
+        return dp[index]=max(pick, not_pick); 
+    }
+    int rob(vector<int>&nums){
+    int n=nums.size();
+        vector<int>dp(n, -1);
+        return f(n-1, nums, dp);
+    }
+};
