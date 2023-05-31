0c5fdd42ad00b87ca5b7f4360833bb5bad7966be
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-06-01 00:16:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.49%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int>v;
+        for(int i=1; i<=n; i++){
+            if(n%i==0)
+                v.push_back(i);
+        }
+        if(k>v.size())
+            return -1;
+        return v[k-1];
+    }
+};
