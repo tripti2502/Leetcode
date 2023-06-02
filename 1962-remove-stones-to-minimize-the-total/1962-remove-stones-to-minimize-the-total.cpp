02837b48bf2dc799b7e562e3b08b571e69c6ac76
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-06-03 01:10:31 +0530

    Time: 836 ms (9.83%), Space: 105 MB (48.83%) - LeetHub

---
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        int n=piles.size();
+        priority_queue<int> pq;
+        for(int i=0; i<n; i++){
+            pq.push(piles[i]);
+        }
+        while(!pq.empty() && k){
+            int top=pq.top();
+            pq.pop();
+            pq.push(((top+1)/2));
+            k=k-1;
+        }
+        int sum=0; 
+        while(!pq.empty()){
+            sum=sum+pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
