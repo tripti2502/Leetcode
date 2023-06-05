bd2cbe86122e23ed16b4b602d9b34fa0449d1068
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-06-05 20:02:28 +0530

    Time: 8 ms (73.14%), Space: 10.1 MB (29.11%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -6,10 +6,11 @@ public:
         // dx=coordinates[1][0] - coordinates[0][0];
             for(i=1;i<coordinates.size();i++)
             {
-                dy=coordinates[i][1] - coordinates[i-1][1];
                 dx=coordinates[i][0] - coordinates[i-1][0];
-                ndy=coordinates[i][1] - coordinates[0][1];  
+                dy=coordinates[i][1] - coordinates[i-1][1];
                 ndx=coordinates[i][0] - coordinates[0][0];  
+                ndy=coordinates[i][1] - coordinates[0][1];  
+               
                 if(ndx * dy != ndy * dx) // if (x2-x1)*(y1-y0) = (x1-x0)(y2-y1), then a straight line, otherwise not
                     return false;
             }
