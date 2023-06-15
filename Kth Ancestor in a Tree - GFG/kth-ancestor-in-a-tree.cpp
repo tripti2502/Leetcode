0f2507fd04559ec477d812c2a164c01d1ccfed9d
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-06-15 15:35:24 +0530

    Added solution - LeetHub

--- Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
@@ -103,7 +103,7 @@ int main()
 
 //User function Template for C++
 /*
-Structure of the node of the binary tree is asNode
+Structure of the node of the binary tree is as
 struct Node
 {
 	int data;
@@ -111,45 +111,36 @@ struct Node
 };
 */
 // your task is to complete this function
-Node* solve(Node* root, int &k, int node){
-    if(root==NULL)
-    return NULL;
-    
-    if(root->data==node){
-        return root;
-    }
+Node* solve(Node *root, int &k, int node){
+    if(root==NULL) return NULL;
+    if(root->data==node) return root;
     
     Node* leftAns=solve(root->left, k, node);
     Node* rightAns=solve(root->right, k, node);
     
-    if(leftAns != NULL && rightAns == NULL){
+    if(leftAns!=NULL && rightAns==NULL){
         k--;
         if(k<=0){
-            k=INT_MAX;
-            return root;
+        k=INT_MAX;
+        return root;
         }
-        
         return leftAns;
     }
-    
-    if(leftAns == NULL && rightAns != NULL){
+    if(leftAns==NULL && rightAns!=NULL){
         k--;
-        if(k<=0){
-            k=INT_MAX;
-            return root;
+        if (k<=0){
+        k=INT_MAX;
+        return root;
         }
         return rightAns;
     }
-    
     return NULL;
-    
 }
+
 int kthAncestor(Node *root, int k, int node)
 {
     Node* ans = solve(root, k, node);
-    if(ans==NULL || ans->data==node)
-    return -1;
-    else
+    if(ans==NULL || ans->data==node) return -1;
+    else 
     return ans->data;
-    
 }
