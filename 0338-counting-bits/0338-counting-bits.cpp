2bdf8bdc623310ba2cd868cffc748c7dd9af8aff
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-06-29 00:24:20 +0530

    Time: 10 ms (32.73%), Space: 8.5 MB (34.00%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+//         vector<int> t(n+1);
+//         t[0] = 0;
+//          for(int i = 1; i<=n; ++i) 
+//             t[i] = t[i/2] + i%2;
+        
+//         return t;
+
+       
+        vector<int> res(n);
+        res.push_back(0);  // for num = 0
+        if (n == 0) return res;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) 
+                res[i] = res[i / 2];
+            else
+                res[i] = res[i - 1] + 1;
+        }
+        return res;
+    }
+};
