110f1b15a2510f64b62b8f682db6bd77d11e99a8
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-07-13 22:52:22 +0530

    Time: 12 ms (84.94%), Space: 10.3 MB (56.15%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -14,3 +14,46 @@ public:
         return a;
     }
 };
+
+// 	// initailize a vector of strings to return ans.
+//     vector<string>ans;
+//     priority_queue<pair<int,int>>pq;
+    
+//     for(int i=0; i<score.size(); i++)
+//     {
+//         pq.push({score[i],i});
+//     }
+// 	// after inserting the scores in priority queue with their index.
+// 	// everytime we pop the top element from priority queue we will get the top scorer as compared to other in priority queue.
+	
+//     int idx;
+// 	// assigning their positions according to the index.
+//      if(!pq.empty())
+//      {
+//          idx = pq.top().second;
+//          ans[idx] = "Gold Medal";
+//          pq.pop();
+//      }
+//      if(!pq.empty())
+//      {
+//          idx = pq.top().second;
+//          ans[idx] = "Silver Medal";
+//          pq.pop();
+//      }
+//      if(!pq.empty())
+//      {
+//          idx = pq.top().second;
+//          ans[idx] = "Bronze Medal";
+//          pq.pop();
+//      }
+//     int x = 4;
+//      while(!pq.empty())
+//      {
+//          idx = pq.top().second;
+//          ans[idx] = to_string(x);
+//          pq.pop();
+//          x++;
+//      }
+	
+//     return ans;
+// }
