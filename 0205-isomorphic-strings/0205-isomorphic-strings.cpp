07fb5f9162aee045b6c1913bb2a22e109d70687c
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-07-16 17:03:26 +0530

    Time: 7 ms (72.73%), Space: 6.8 MB (88.88%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int m1[256] = {0}, m2[256] = {0}, n = s.size();
-        for (int i = 0; i < n; i++) {
-            if (m1[s[i]] != m2[t[i]]) return false;
-            m1[s[i]] = i+1 ;
-            m2[t[i]] = i+1;
-        }
-        return true;
+//         int m1[256] = {0}, m2[256] = {0}, n = s.size();
+//         for (int i = 0; i < n; i++) {
+//             if (m1[s[i]] != m2[t[i]]) return false;
+//             m1[s[i]] = i+1 ;
+//             m2[t[i]] = i+1;
+//         }
+//         return true;
+//     }
+// };
+
+int m=s.size();
+int n=t.size();
+if(m!=n) return false;
+int m1[256]={};
+int m2[256]={};
+for(int i=0; i<n; i++){
+    if(!m1[s[i]] && !m2[t[i]]){
+        m1[s[i]]=t[i];
+        m2[t[i]]=s[i];
     }
+    else if (m1[s[i]]!= t[i]) return false;
+    
+}
+return true;
+}
 };
