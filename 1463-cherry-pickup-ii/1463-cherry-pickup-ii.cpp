5a6dc7762f5e13d7243badbeb25728da998b183b
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-07-24 22:48:19 +0530

    Time: 173 ms (38.60%), Space: 16.9 MB (66.89%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f (int i, int j1, int j2, int r, int c,vector<vector<int>>& grid,
+    int f(int i, int j1, int j2, int r, int c,vector<vector<int>>& grid,
            vector<vector<vector<int>>> &dp){
         if(j1<0 || j2<0 || j1>=c ||j2>=c){
          return -1e8;
@@ -10,7 +10,6 @@ public:
             else
                 return grid[i][j1]+grid[i][j2];
         }
-        
         if(dp[i][j1][j2]!= -1) return dp[i][j1][j2];
         int maxi=0;
         for(int dj1=-1; dj1 <= +1; dj1++){
