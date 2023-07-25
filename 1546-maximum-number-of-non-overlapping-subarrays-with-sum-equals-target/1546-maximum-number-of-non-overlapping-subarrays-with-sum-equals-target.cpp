3638813b6d0cb12c71f4e1f548b735eb99b0ca84
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-07-25 10:07:20 +0530

    Time: 202 ms (99.64%), Space: 78 MB (98.55%) - LeetHub

--- 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -1,19 +1,43 @@
-class Solution {
+// class Solution {
+// public:
+//     int maxNonOverlapping(vector<int>& nums, int target) {
+//          int ans = 0;
+//          set<int> st;
+//         st.insert(0);
+//          int preSum = 0;
+//          for(int a : nums){
+//              preSum += a;
+//              if(st.find(preSum - target) != st.end()){
+//                  ans++;
+//                  st.clear();
+//                  preSum = 0;
+//              }
+//                st.insert(preSum);
+//          }
+//         return ans;
+//     }
+// };
+ class Solution {
 public:
     int maxNonOverlapping(vector<int>& nums, int target) {
-         int ans = 0;
-         set<int> st;
-         st.insert(0);
-         int preSum = 0;
-         for(int a : nums){
-             preSum += a;
-             if(st.find(preSum - target) != st.end()){
-                 ans++;
-                 st.clear();
-                 preSum = 0;
-             }
-               st.insert(preSum);
-         }
+        unordered_map<int , int> mp;
+        int sum = 0, ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];  // summing up the current element
+            if(mp.find(sum - target )!= mp.end()){ // if target sum is Found
+                ans+= 1; //increament the result
+                mp.clear(); // clear the map (as the sum is useless , as it will create overlapping sum)
+                sum = 0; //make current sum = 0;
+            }
+            else if(sum == target){ //if sum is equal to target
+                sum = 0; 
+                ans+= 1;
+                mp.clear();
+            }
+            else{
+                mp[sum]++; // else store the sum
+            }
+        }
         return ans;
     }
 };
