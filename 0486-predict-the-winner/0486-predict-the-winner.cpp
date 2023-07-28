0e5efc9d0974863ffb944e9748600636b2ef1801
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-07-28 23:30:38 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (43.52%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,7 +1,7 @@
 class Solution {
     int func(int l,int r,vector<int>& nums, vector<vector<int>> &dp){
         if(l>r) return 0;
-        if(l==r) return nums[l];
+        if(l==r) return  nums[l]; 
         if(dp[l][r]!=-1) return dp[l][r];
         int ans=max(nums[l] + min(func(l+2,r,nums,dp),func(l+1,r-1,nums,dp)),nums[r] + min(func(l,r-2,nums,dp),func(l+1,r-1,nums,dp)));
         return dp[l][r]=ans;
