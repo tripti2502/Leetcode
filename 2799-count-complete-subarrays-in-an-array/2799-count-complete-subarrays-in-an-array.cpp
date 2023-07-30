9e1e1cd52597a796ec80c2733a3a31087447218c
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-07-30 10:07:18 +0530

    Time: 1595 ms (60.00%), Space: 478.6 MB (40.00%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& A) {
-        int n = A.size(), k = unordered_set<int>(A.begin(), A.end()).size(), res = 0, i = 0;
-        unordered_map<int, int> count;
-        for (int j = 0; j < n; ++j) {
-            k -= count[A[j]]++ == 0;
-            while (k == 0)
-                k += --count[A[i++]] == 0;
-            res += i;
+    int countCompleteSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> freq;
+        
+        for(int i = 0; i < n; i++) {  //to count distinct ele
+            freq.insert(nums[i]);
         }
-        return res;
+        
+        int distinct = freq.size();
+        int ans = 0;
+    
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> temp;
+
+            for(int j = i; j < n; j++) {
+                temp.insert(nums[j]);
+                if (temp.size() == distinct) {
+                    ans++;
+                }
+            }
+        }
+    
+    return ans;   
     }
 };
+//     int countCompleteSubarrays(vector<int>& A) {
+//         int n = A.size(), k = unordered_set<int>(A.begin(), A.end()).size(), res = 0, i = 0;
+//         unordered_map<int, int> count;
+//         for (int j = 0; j < n; ++j) {
+//             k -= count[A[j]]++ == 0;
+//             while (k == 0)
+//                 k += --count[A[i++]] == 0;
+//             res += i;
+//         }
+//         return res;
+//     }
+// };
