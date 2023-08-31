d15050cc47a6ddef1c132964e8e8b3e1a73885e7
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-08-31 18:42:18 +0530

    Time: 3 ms (74.13%), Space: 9.4 MB (96.49%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,30 +1,40 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int row=matrix.size();
+//         int col=matrix[0].size();
+//         int start=0;
+//         int end= row*col-1;
+//         int mid=start+(end-start)/2;
+//         while(start<=end){
+//             int element=matrix[mid/col][mid%col];
+//             if(element==target){
+//                 return 1;
+//             }
+//             if(element<target){
+//                 start=mid+1;
+//             }
+//             else{
+//                 end=mid-1;
+//             }
+            
+//             mid=start+(end-start)/2;
+            
+//         }
+//             return 0;
+        
+//     }
+// };
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        int start=0;
-        int end= row*col-1;
-        int mid=start+(end-start)/2;
-        while(start<=end){
-            int element=matrix[mid/col][mid%col];
-            if(element==target){
-                return 1;
-            }
-            if(element<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-            
-            mid=start+(end-start)/2;
-            
+        int x = matrix[0].size()-1;
+        int y = 0;
+        while(x>=0 && y<matrix.size()){
+           if(matrix[y][x]==target) return true;
+           if(matrix[y][x]>target) x--;
+           else y++;
         }
-            return 0;
-        
+        return false;
     }
 };
-
-
-
