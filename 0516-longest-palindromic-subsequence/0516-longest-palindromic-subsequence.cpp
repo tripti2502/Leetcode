9b87a339230cf93b1e3b1932754072939f6a860a
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-08-31 13:33:15 +0530

    Time: 169 ms (44.52%), Space: 73 MB (34.00%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -16,26 +16,29 @@ public:
 //         return dp[0][n-1];
 //     }
 // };
-int lcs(string s1, string s2) {
+    int lcs(string s1, string s2) {
     
     int n=s1.size();
     int m=s2.size();
 
-    vector<int> prev(m+1,0), cur(m+1,0);
-    
-    // Base Case is covered as we have initialized the prev and cur to 0.
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
     
     for(int ind1=1;ind1<=n;ind1++){
         for(int ind2=1;ind2<=m;ind2++){
             if(s1[ind1-1]==s2[ind2-1])
-                cur[ind2] = 1 + prev[ind2-1];
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
             else
-                cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
         }
-        prev= cur;
     }
     
-    return prev[m];
+    return dp[n][m];
 }
 
 int longestPalindromeSubseq(string s){
@@ -43,4 +46,32 @@ int longestPalindromeSubseq(string s){
     reverse(s.begin(),s.end());
     return lcs(s,t);
 }
+
+// int lcs(string s1, string s2) {
+    
+//     int n=s1.size();
+//     int m=s2.size();
+
+//     vector<int> prev(m+1,0), cur(m+1,0);
+    
+//     // Base Case is covered as we have initialized the prev and cur to 0.
+    
+//     for(int ind1=1;ind1<=n;ind1++){
+//         for(int ind2=1;ind2<=m;ind2++){
+//             if(s1[ind1-1]==s2[ind2-1])
+//                 cur[ind2] = 1 + prev[ind2-1];
+//             else
+//                 cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
+//         }
+//         prev= cur;
+//     }
+    
+//     return prev[m];
+// }
+
+// int longestPalindromeSubseq(string s){
+//     string t = s;
+//     reverse(s.begin(),s.end());
+//     return lcs(s,t);
+// }
 };
