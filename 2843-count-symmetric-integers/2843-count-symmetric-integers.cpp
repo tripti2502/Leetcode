c70018b36d428654dcb8ca2e859df48c577d5414
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-09-03 10:32:28 +0530

    Time: 149 ms (100.00%), Space: 6 MB (60.00%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
@@ -1,18 +1,51 @@
 class Solution {
 public:
-    int countSymmetricIntegers(int low, int high) {
-        int ans = 0;
-    for(int i = low; i <= high; ++i){
-        string s = to_string(i);
-        int t = 0, n = s.size();
-        for(int j = 0; j < n/2; ++j) 
-        { 
-            t += s[j] - s[n - j - 1]; 
-        }
-        if(n%2 == 0 && t == 0) 
-            ans++;
-    }
-    return ans;
+//     int countSymmetricIntegers(int low, int high) {
+//         int ans = 0;
+//     for(int i = low; i <= high; ++i){
+//         string s = to_string(i);
+//         int t = 0, n = s.size();
+//         for(int j = 0; j < n/2; ++j) 
+//         { 
+//             t += s[j] - s[n - j - 1]; 
+//         }
+//         if(n%2 == 0 && t == 0) 
+//             ans++;
+//     }
+//     return ans;
         
+//     }
+// };
+
+
+
+bool isSymmetric(int num) {
+    string s = to_string(num);
+    int n = s.length();
+    
+    // If the number of digits is odd, it can't be symmetric
+    if (n % 2 != 0)
+        return false;
+
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < n / 2; i++) {
+        sum1 += (s[i] - '0');
+        sum2 += (s[n - 1 - i] - '0');
     }
+
+    return sum1 == sum2;
+}
+
+int countSymmetricIntegers(int low, int high) {
+    int count = 0;
+    
+    for (int num = low; num <= high; num++) {
+        if (isSymmetric(num)) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
 };
