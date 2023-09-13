56eef61b19facec1e80dc90edabcbc44e8176ad4
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-09-13 15:46:45 +0530

    Time: 10 ms (69.01%), Space: 8.9 MB (11.94%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     string frequencySort(string s) 
     {
-        int cnt[128] = {0};
-        for (char c : s)
-            cnt[c]++;
-        auto cmp = [&](char a, char b)
+        // int cnt[128] = {0};
+        // for (char c : s)
+        //     cnt[c]++;
+        // auto cmp = [&](char a, char b)
+        // {
+        //     return cnt[a] > cnt[b] || (cnt[a] == cnt[b] && a < b); 
+        // };
+        // sort(s.begin(), s.end(), cmp);
+        // return s;
+        string ans= "";
+        
+        unordered_map<char,int> freq;         
+        priority_queue<pair<int,char>> pq; 
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            pq.push({it.second,it.first}); 
+           
+        
+           while(!pq.empty())
         {
-            return cnt[a] > cnt[b] || (cnt[a] == cnt[b] && a < b); 
-        };
-        sort(s.begin(), s.end(), cmp);
-        return s;
+            int n = pq.top().first;
+            while(n--)
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
     }
 };
