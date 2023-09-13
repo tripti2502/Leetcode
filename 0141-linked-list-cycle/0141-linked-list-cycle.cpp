a7b7ce01b053d509da7d73a63177bb84a995a8a8
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-09-13 20:47:19 +0530

    Time: 14 ms (14.44%), Space: 10.8 MB (8.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,22 +8,35 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *iList) {
-        auto a=iList,b=iList;
-        if (a==NULL or a->next==NULL)
-            return false;
+    bool hasCycle(ListNode *h) {
+//         auto a=iList,b=iList;
+//         if (a==NULL or a->next==NULL)
+//             return false;
 
-        a=a->next;
-        b=b->next->next;
+//         a=a->next;
+//         b=b->next->next;
 
-        while ((a!=b) && a && b)
-        {
-            a=a->next;
-            if(b->next==NULL)
-                return false;
-            b=b->next->next;
-        }
+//         while ((a!=b) && a && b)
+//         {
+//             a=a->next;
+//             if(b->next==NULL)
+//                 return false;
+//             b=b->next->next;
+//         }
 
-        return a==b;
+//         return a==b;
+        
+        unordered_set<ListNode*> s;
+    while (h != NULL) {
+        // If this node is already present in hashmap it means there is a cycle
+    
+        if (s.find(h) != s.end())
+            return true;
+        // If we are seeing the node for the first time, insert it in hash
+        s.insert(h);
+        h = h->next;
+    }
+    return false;
+
     }
 };
