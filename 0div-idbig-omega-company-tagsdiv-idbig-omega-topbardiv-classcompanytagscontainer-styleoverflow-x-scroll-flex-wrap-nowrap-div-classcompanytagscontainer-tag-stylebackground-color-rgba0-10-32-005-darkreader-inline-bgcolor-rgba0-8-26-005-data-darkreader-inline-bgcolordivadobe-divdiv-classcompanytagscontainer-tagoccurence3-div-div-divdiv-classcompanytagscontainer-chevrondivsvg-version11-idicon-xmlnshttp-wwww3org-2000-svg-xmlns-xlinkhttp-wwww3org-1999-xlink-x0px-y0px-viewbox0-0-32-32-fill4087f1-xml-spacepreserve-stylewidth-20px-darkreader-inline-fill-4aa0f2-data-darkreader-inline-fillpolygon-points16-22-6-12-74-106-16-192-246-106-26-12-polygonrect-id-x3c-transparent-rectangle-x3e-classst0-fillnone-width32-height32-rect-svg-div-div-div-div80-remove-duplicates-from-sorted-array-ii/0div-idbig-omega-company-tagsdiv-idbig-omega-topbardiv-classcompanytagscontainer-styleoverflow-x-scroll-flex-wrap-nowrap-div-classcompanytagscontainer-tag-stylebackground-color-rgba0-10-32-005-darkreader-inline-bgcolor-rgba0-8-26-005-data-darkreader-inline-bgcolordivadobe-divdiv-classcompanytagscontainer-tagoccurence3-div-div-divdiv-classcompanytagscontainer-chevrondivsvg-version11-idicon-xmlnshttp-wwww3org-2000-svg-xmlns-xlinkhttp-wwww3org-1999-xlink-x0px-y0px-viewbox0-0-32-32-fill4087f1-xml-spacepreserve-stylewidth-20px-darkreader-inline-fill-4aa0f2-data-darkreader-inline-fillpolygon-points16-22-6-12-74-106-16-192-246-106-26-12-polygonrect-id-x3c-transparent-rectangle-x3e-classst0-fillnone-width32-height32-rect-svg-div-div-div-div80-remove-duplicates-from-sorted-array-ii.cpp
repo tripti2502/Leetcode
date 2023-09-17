2d2c7f228f3e72290e787f3c9a5143a0cdea1cfe
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-09-17 09:42:40 +0530

    Time: 4 ms (66.27%), Space: 11.3 MB (12.18%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivadobe-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div80-remove-duplicates-from-sorted-array-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivadobe-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div80-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,28 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//     int x = 0;
+//     for (int n : nums)
+//         if (x < 2 || n > nums[x-2])
+//             nums[x++] = n;
+//     return x;
+// }
+// };
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n =nums.size();
+        if(n < 3) return n;
+        
+        int indx = 2;
+        for(int i =2; i< nums.size(); i++){
+             if(nums[i] != nums[indx -2]){
+                 nums[indx] = nums[i];
+                 indx++;
+             }
+        }
+        return indx;
+  }
+};
+
