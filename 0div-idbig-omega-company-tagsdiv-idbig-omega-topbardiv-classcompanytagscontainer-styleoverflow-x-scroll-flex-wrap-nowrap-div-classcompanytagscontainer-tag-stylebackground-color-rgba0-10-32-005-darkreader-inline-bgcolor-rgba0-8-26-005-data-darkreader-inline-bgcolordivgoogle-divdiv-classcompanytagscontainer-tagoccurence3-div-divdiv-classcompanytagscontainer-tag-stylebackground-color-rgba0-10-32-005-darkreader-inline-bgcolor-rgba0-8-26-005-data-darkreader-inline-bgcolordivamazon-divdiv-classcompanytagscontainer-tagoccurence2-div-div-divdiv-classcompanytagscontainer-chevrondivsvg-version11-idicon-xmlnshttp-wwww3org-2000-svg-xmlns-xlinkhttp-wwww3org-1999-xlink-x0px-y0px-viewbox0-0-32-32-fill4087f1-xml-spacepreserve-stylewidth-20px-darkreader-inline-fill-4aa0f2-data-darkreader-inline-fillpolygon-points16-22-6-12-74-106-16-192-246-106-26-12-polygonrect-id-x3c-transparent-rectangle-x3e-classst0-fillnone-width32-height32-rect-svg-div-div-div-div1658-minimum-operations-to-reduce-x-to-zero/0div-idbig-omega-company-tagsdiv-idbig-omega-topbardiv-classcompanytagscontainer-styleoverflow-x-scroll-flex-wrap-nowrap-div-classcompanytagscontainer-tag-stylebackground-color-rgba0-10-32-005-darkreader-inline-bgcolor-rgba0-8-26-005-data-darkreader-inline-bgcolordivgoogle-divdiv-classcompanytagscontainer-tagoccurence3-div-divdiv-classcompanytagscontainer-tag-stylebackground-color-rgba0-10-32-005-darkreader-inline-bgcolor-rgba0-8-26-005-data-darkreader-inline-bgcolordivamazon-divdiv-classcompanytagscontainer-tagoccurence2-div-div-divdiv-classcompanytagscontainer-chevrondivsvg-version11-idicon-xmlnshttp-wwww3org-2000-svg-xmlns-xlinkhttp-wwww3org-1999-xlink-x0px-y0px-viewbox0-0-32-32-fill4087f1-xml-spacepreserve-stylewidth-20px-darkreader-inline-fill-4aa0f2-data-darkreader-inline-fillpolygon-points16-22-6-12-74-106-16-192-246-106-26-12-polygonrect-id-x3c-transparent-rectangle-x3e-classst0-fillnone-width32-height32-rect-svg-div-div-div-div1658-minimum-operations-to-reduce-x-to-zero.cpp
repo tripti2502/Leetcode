72e081a8b29f145699c57c4e4f01b9f62054c2b6
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-09-20 19:25:29 +0530

    Time: 497 ms (8.83%), Space: 209.5 MB (5.04%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1658-minimum-operations-to-reduce-x-to-zero/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        unordered_map<int, int>mp;
+        int n=nums.size();
+         if(nums[0]==x || nums[n-1]==x) return 1;
+        
+        int ans=INT_MAX;
+        int prefix[n];
+        prefix[0]=nums[0];
+            for(int i=1; i<n; i++)
+            {
+                prefix[i]=nums[i]+prefix[i-1];
+                
+        mp[prefix[i]]=i+1;
+            if(prefix[i]==x)
+               ans = min(ans, i + 1);
+            }
+        
+        int suffix[n+1];
+        suffix[n]=0;
+            for(int i=n-1; i>=0; i--)
+            {
+                suffix[i]=nums[i]+suffix[i+1];
+        
+        
+            if(suffix[i]==x)
+               ans = min(ans, n-i);
+                
+                if(mp[x - suffix[i]] > 0 && i + 1 > mp[x - suffix[i]]) 
+				ans = min(ans, n - i + mp[x - suffix[i]]);
+        
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
