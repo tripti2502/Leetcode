60b5d33489b8e13394a5c9f7c0fbd1e5cd0d31ab
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-10-06 12:07:01 +0530

    Added solution - LeetHub

---
+++ Design HashMap - GFG/design-hash-map.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    vector<int>v;
+    MyHashMap() {
+        v.resize(1e5, -1);
+    }
+
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        v[key]=value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return v[key];
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        v[key]=-1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        MyHashMap obj;
+        int q;
+        cin >> q;
+        while (q--) {
+            string s;
+            cin >> s;
+            if (s == "put") {
+                int key, val;
+                cin >> key >> val;
+                obj.put(key, val);
+            }
+            else if ( s == "remove") {
+                int key;
+                cin >> key;
+                obj.remove(key);
+            }
+            else {
+                int key;
+                cin >> key;
+                cout << obj.get(key) << endl;
+            }
+        }
+}
+    return 0;
+}
+
+// } Driver Code Ends
