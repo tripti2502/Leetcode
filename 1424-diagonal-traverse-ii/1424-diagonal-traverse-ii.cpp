785c94d58a4bea72e367c016a75c5916ac30f96b
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2023-11-23 00:00:42 +0530

    Time: 261 ms (24.36%), Space: 109 MB (9.83%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+       map<int,vector<int>>mp;
+        int sum=0;
+       
+            for(int i=0; i<nums.size(); i++){
+           for(int j=0; j<nums[i].size(); j++){
+               mp[i+j].push_back(nums[i][j]);
+           }
+        }
+        
+         vector<int> ans;
+                                                               
+    for(auto it: mp)                                          
+    {                                                         
+        reverse(it.second.begin(),it.second.end());           
+                                                              
+        for(auto k: it.second)                               
+        {
+            ans.push_back(k);
+        }
+    }
+    return ans;
+    }
+};
+
