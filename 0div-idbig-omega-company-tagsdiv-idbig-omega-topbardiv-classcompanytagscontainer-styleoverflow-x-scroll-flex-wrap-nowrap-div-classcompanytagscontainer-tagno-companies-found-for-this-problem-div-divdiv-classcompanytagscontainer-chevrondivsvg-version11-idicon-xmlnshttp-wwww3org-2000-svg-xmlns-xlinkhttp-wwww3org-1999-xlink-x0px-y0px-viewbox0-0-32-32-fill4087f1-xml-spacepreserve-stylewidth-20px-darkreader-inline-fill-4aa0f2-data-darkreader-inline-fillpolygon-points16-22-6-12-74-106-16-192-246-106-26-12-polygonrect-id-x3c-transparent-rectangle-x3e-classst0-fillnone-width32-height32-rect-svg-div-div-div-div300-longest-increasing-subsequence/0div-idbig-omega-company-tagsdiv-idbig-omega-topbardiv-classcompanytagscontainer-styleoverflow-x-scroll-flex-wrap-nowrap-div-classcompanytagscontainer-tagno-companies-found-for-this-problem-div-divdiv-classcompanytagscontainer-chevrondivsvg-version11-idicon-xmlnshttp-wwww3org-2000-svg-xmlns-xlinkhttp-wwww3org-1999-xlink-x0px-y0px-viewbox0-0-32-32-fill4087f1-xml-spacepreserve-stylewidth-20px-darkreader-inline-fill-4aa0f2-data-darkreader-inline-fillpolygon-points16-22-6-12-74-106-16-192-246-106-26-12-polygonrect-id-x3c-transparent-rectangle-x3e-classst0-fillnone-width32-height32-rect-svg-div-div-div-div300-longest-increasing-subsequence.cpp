1a425b995de9296c05e1355c7f67df09fcc348e8
Tripti Tiwari <114186679+tripti2502@users.noreply.github.com>
2024-01-05 15:22:07 +0530

    Time: 1034 ms (5.45%), Space: 292.7 MB (11.08%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int helper(int idx,int prev,vector<int>& nums,vector<vector<int>> &dp){
+        if(idx==0) return 0;
+        if(dp[idx][prev]!=-1) return dp[idx][prev];
+        int notTake = helper(idx-1,prev,nums,dp);
+        int take = 0;
+        
+            take = 1+helper(idx-1,idx,nums,dp);
+        
+        return dp[idx][prev]=max(take,notTake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+2,0));
+        for(int idx=1;idx<=nums.size();idx++){
+            for(int prev=1;prev<=nums.size()+1;prev++){
+                int notTake = dp[idx-1][prev];
+                int take = 0;
+                if(prev==nums.size()+1 || nums[idx-1]<nums[prev-1]){
+                    take = 1+dp[idx-1][idx];
+                }
+                dp[idx][prev]=max(take,notTake);        
+            }
+        }
+        
+        return dp[nums.size()][nums.size()+1];
+        return helper(nums.size(),nums.size()+1,nums,dp);
+    }
+};
